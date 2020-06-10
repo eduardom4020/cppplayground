463e90ecd31b81d6b4f2d5dafb9e0b4e56961a34
Eduardo Melo <eduardo.melo@ivia.com.br>
2020-06-09 21:57:08 -0300

    feat - finished convexhull logics and export

--- src/main.cpp
+++ src/main.cpp
@@ -68,6 +68,47 @@ int main(int argc, char const *argv[])
     }
     std::cout << "Hull size " << hull.size() << std::endl;
     convexHull.close();
+
+    std::ofstream convexHullExport;
+    convexHullExport.open ("convex-hull.hull");
+
+    for(auto* point : pointsCloud)
+    {
+      convexHullExport << "p " << point->x << " " << point->y << " " << point->z << "\n";
+    }
+
+    convexHullExport << "h 1" << "\n";
+    for(auto& face : hull)
+    {
+      int p1Index = -1;
+      int p2Index = -1;
+      int p3Index = -1;
+
+      for(int i=0; i < pointsCloud.size(); i++)
+      {
+        if(pointsCloud[i] == face.points[0])
+        {
+          p1Index = i + 1;
+        }
+
+        if(pointsCloud[i] == face.points[1])
+        {
+          p2Index = i + 1;
+        }
+
+        if(pointsCloud[i] == face.points[2])
+        {
+          p3Index = i + 1;
+        }
+      }
+
+      if(p1Index >= 0 && p2Index >= 0 && p3Index >= 0)
+      {
+        convexHullExport << "f " << p1Index << " " << p2Index << " " << p3Index << "\n";
+      }
+    }
+
+    convexHullExport.close();
   }
   catch(char const* exception)
   {
